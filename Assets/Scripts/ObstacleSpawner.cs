2c4ba3ee1f9c2a2a8cee6ceb4fbe827f620d97d0
Obbie <obipras@gmail.com>
2021-10-07 19:22:35 +0700

    Problem-7

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject obstaclePrefabs;
+    private float maxYPosition = -3.8f;
+    private float maxXPosition = -8;
+
+    private void Start()
+    {
+        InvokeRepeating ("SpawnObstacle", 1, 2);
+    }
+
+    void SpawnObstacle()
+    {
+        float xPosition = Random.Range(-maxXPosition, maxYPosition);
+        float yPosition = Random.Range(-maxYPosition, maxXPosition);
+
+        GameObject obstacle = Instantiate(obstaclePrefabs);
+        obstacle.transform.position = new Vector3 (xPosition, yPosition, 1);
+    }
+}
