af4b8e45027b02abecbbdec6a965b877f38716be
Obbie <obipras@gmail.com>
2021-10-07 14:33:36 +0700

    Problem_2

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D p_rigidbody;
+    // Start is called before the first frame update
+    
+    private void Awake()
+    {
+        p_rigidbody = GetComponent<Rigidbody2D>();
+    }
+    
+    void Start()
+    {
+        p_rigidbody.AddForce(new Vector2(1,1) * moveSpeed);
+    }
+}
